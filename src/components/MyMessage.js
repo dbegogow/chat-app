dac881de17c7902216aeb39e324e5a8ec9cc6012
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-30 16:09:01 +0300

    Implement MyMessage component

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,7 +1,27 @@
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+    if (message?.attachments?.length > 0) {
+        return (
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right' }}
+            />
+        );
+    }
+
     return (
-        <div>
-            My Message
+        <div
+            className="message"
+            style={
+                {
+                    float: 'right',
+                    marginRight: '18px',
+                    color: '#FFF',
+                    background: '#A29422'
+                }
+            }>
+            {message.text}
         </div>
     )
 };
