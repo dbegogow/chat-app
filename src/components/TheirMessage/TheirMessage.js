afd0cdc981784995cdcd6f049de447deebaf0ab2
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-06-30 17:19:38 +0300

    Implement TheirMessage component

--- src/components/TheirMessage/TheirMessage.js
+++ src/components/TheirMessage/TheirMessage.js
@@ -1,8 +1,49 @@
-const TheirMessage = () => {
+import styles from './TheirMessage.module.css';
+
+const TheirMessage = ({
+    lastMessage,
+    message
+}) => {
+    const isFirstMessageByUser = !lastMessage
+        || lastMessage.sender.username !== message.sender.username;
+
+
+
     return (
-        <div>
-            Their Message
-        </div>
+        <div className={styles.messageRow}>
+            {isFirstMessageByUser && (
+                <div
+                    className={styles.messageAvatar}
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+            {
+                message?.attachments?.length > 0
+                    ? (
+                        <img
+                            src={message.attachments[0].file}
+                            alt="message-attachment"
+                            className={styles.messageImage}
+                            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                        />
+                    )
+                    : (
+                        <div>
+                            <div
+                                className={styles.message}
+                                style={
+                                    {
+                                        float: 'left',
+                                        background: '#F4F2B0',
+                                        marginLeft: isFirstMessageByUser ? '4px' : '48px'
+                                    }
+                                }>
+                                {message.text}
+                            </div>
+                        </div>
+                    )
+            }
+        </div >
     )
 };
 
